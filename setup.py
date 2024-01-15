4ddab620c3b9680c56b36d221871f37c819c2d1a
Benji <97198967+BenPI88@users.noreply.github.com>
2024-01-15 12:45:29 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,4 @@
+print("Ultimate Moderation Plugin Pack Setup")
+servername = input("What is the name of your server?")
+print("Updating configs...")
+print("Done! Drag all files from the output folder to your plugins directory.")
